82d867fe7f9e81666cc4fff4348425c820c2190b
Pahsa1 <93053312+Pahsa1@users.noreply.github.com>
2022-09-08 22:39:37 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,42 @@
+﻿
+#include <iostream>
+#include <math.h>
+#include <conio.h>
+#include <stdio.h>
+#pragma hdrstop
+#include <cmath>
+using  namespace std;
+
+int main()
+{
+    double x, y, z, a, b, c, g, t, q;
+    cout << " \n x = "; cin >> x;
+    cout << " \n y = "; cin >> y;
+    cout << " \n z = "; cin >> z;
+
+
+    if (x > 1 or x < -1) {
+        
+        cout << "\n x cannot be more than 1 or less than -1";
+        cout << "\n ";
+        return 1;
+    }
+
+    q = (abs(x-y)*z) + x*x;
+
+    if (q == 0) {
+        cout << "\n such a set of numbers isn't possible";
+        cout << "\n ";
+            return 1;
+    }
+
+    a = 5 * atan(x);
+    b = 0.25 * acos(x);
+    c = (x + (3 * abs(x - y)) + (x * x));
+    g = (abs(x-y)*z) + (x*x);
+    t =a-(b*c/g);
+    cout << "\n t = " << t << endl;
+    
+}
+
+
---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <iostream>
+#include <math.h>
+#include <conio.h>
+#include <stdio.h>
+#pragma hdrstop
+#include <cmath>
+using  namespace std;
+
+int main()
+{   
+    double x, y, z, a, е, c, g, t, n, f, b, e, k;
+    cout << " \n x = "; cin >> x;
+    cout << " \n y = "; cin >> y;
+    cout << " \n z = "; cin >> z;
+    
+    if (x == 0) {
+        cout << "\n x cannot be 0";
+        cout << "\n ";
+        return 1;
+    }
+
+    a = pow(abs(x), 1.0 / 3.0);
+    t = pow(y, a);
+    c = pow(cos(y), 3.0);
+    k = pow((x + y), 1.0 / 2.0);
+    g = (abs(x - y)) * (1 + ((pow(sin(z), 2))) / (pow((x + y), 1.0 / 2.0)));
+    f = abs(x - y);
+    n = exp(f);
+    e = n + x / 2.0;
+    b = t + ((c * g) / e);
+
+    if (e == 0) {
+        cout << "\n such a set of numbers is impossible";
+        cout << "\n ";
+        return 1;
+    }
+
+
+    if (k == 0) {
+        cout << "\n such a set of numbers is impossible";
+        cout << "\n ";
+        return 1;
+    }
+   
+    cout << "\n b = " << b << endl;
+
+}
+
+
